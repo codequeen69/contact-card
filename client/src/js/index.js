0e1cd1ecc5a31f92b197d7a36e16fad7bd23b8c6
Venus DM <crhowe90@gmail.com>
2022-09-27 19:15:51 -0500

    render functionality

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,11 +1,12 @@
-//import modules
-import './form';
-import './submit';
-
 import { initDb, getDb, postDb } from './database';
 import { Tooltip, Toast, Popover } from 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { fetchCards } from './cards';
+import { toggleForm } from './form';
+import { clearForm } from './form';
 
+//import modules
+import './form';
 
 
 //import images
@@ -17,14 +18,49 @@ import Otter from '../images/otter-logo.jpg'
 //add images on load
 window.addEventListener('load', function() {
   initDb();
-  getDb();
-  postDb("Lernantino", "learnantino@test.com", 8186601234, "Bear");
-  getDb();
+  fetchCards();
   document.getElementById('logo').src = Logo;
   document.getElementById('bearThumbnail').src = Bear;
   document.getElementById('dogThumbnail').src = Dog;
   document.getElementById('logo').src = Otter;
 });
+
+// Form functionality
+const form = document.getElementById("formToggle");
+const newContactButton = document.getElementById("new-contact");
+let submitBtnToUpdate = false;
+let profileId;
+
+newContactButton.addEventListener('click', event => {
+  toggleForm()
+ })
+
+form.addEventListener('submit', event => {
+  // Handle data
+  event.preventDefault();
+let name = document.getElementById("name").value;
+let phone = document.getElementById("phone").value;
+let email = document.getElementById("email").value;
+let profile = document.querySelector('input[type="radio"]:checked').value;
+
+  // Post form data to IndexedDB OR Edit an existing card in IndexedDB
+if (submitBtnToUpdate == false) {
+  postDb(name, email, phone, profile);
+} else {
+
+  fetchCards();
+    // Toggles the submit button back to POST functionality
+  submitBtnToUpdate = false;
+}
+
+// Clear form
+clearForm();
+// Toggle form
+toggleForm();
+// Reload the DOM
+fetchCards();
+});
+
 // Import CSS files
 import "../css/index.css";
   
